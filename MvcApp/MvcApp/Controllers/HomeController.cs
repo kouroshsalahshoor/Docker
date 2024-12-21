7fa0f9cf0dea139690573a328d68c223a1ffc482
kourosh <kourosh23@hotmail.com>
2024-12-21 07:28:13 +0100

    nvcapp with docker

--- MvcApp/MvcApp/Controllers/HomeController.cs
+++ MvcApp/MvcApp/Controllers/HomeController.cs
@@ -1,16 +1,10 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MvcApp.Models;
-<<<<<<< HEAD
 using MvcApp.Repository;
-=======
->>>>>>> mvcapp
 
 namespace MvcApp.Controllers
 {
     public class HomeController : Controller
     {
-<<<<<<< HEAD
         private IRepository repository;
         private string message;
         public HomeController(IRepository repo, IConfiguration config)
@@ -22,29 +16,6 @@ namespace MvcApp.Controllers
         {
             ViewBag.Message = message;
             return View(repository.Products);
-=======
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
->>>>>>> mvcapp
         }
     }
 }
--- MvcApp/MvcApp/Program.cs
+++ MvcApp/MvcApp/Program.cs
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 using MvcApp.Repository;
 
-=======
->>>>>>> mvcapp
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-<<<<<<< HEAD
 builder.Services.AddTransient<IRepository, DummyRepository>();
 
-=======
->>>>>>> mvcapp
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,6 +28,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
